b4864eda7539c66ee32ccc07f571e0d9ddf15ba1
melike <milteralp@gmail.com>
2021-03-02 21:55:48 +0300

    Fixed class name

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,3 @@
-from .lake_base_dataset import LakeBaseDataset
+from .base_lake_dataset import BaseLakeDataset
 from .lake_1d_dataset import Lake1dDataset
 from .lake_2d_dataset import Lake2dDataset
