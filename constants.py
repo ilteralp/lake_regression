ecf5e41293e018f48e894c7e8b7a48a9bce7e28d
melike <milteralp@gmail.com>
2021-03-02 21:52:19 +0300

    Added model path. Converted month and year to class format

--- constants.py
+++ constants.py
@@ -15,10 +15,12 @@ ROG_ENV = not LOCAL_ENV
 """ Melike local """
 if LOCAL_ENV:
     ROOT_DIR = '/home/melike/rs/balik_golu'
+    MODEL_DIR_PATH = '/home/melike/repos/lake_regression/model_files'
     
 """ ROG """
 if ROG_ENV:
     ROOT_DIR = '/home/rog/rs/balik_golu'
+    MODEL_DIR_PATH = '/home/rog/repos/lake_regression/model_files'
     
 MASK_PATH = osp.join(ROOT_DIR, 'lake_mask.png')
 IMG_DIR_PATH = osp.join(ROOT_DIR, 'balik')
@@ -28,4 +30,8 @@ LABELED_INDICES = ([537, 427, 340, 263, 165, 107, 172, 249, 337, 447],
                     [235, 280, 325, 345, 398, 342, 298, 262, 225, 197])          # Or maybe vice versa ?    
 IMG_SHAPE = [12, 650, 650]
 SEASONS = {"'spring'" : 0, "'summer'" : 1, "'autumn'" : 2, "'winter'" : 3}
-# LABELED_INDICES = ([537, 427], [235, 280])
+YEARS = {'2017' : 0, '2018' : 1, '2019' : 2}
+DATE_TYPES = {'month' : 0, 'season' : 1, 'year' : 2}
+
+""" ===================== Model params ===================== """
+BATCH_SIZE = 4
