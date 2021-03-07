25d3e35fc8d04bc726ddf1e7dc8b9ce3fe17b2e8
melike <milteralp@gmail.com>
2021-03-07 17:22:12 +0300

    Testing without seed

--- train.py
+++ train.py
@@ -412,7 +412,7 @@ def train_on_folds(model, dataset, unlabeled_dataset, train_fn, loss_fn_class, l
         
 if __name__ == "__main__":
     
-    seed = 42
+    seed = None
     if seed is not None:
         torch.manual_seed(seed)
         np.random.seed(seed)
