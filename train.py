aa077f38b4d9f76cdaabb7e998572fad5ebcf95b
melike <milteralp@gmail.com>
2021-03-16 19:36:12 +0300

    Testing with 100 epochs

--- train.py
+++ train.py
@@ -590,7 +590,7 @@ if __name__ == "__main__":
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")     # Use GPU if available
     args = {'num_folds': None,
-            'max_epoch': 10,
+            'max_epoch': 100,
             'device': device,
             'seed': seed,
             'create_val': True,                                                 # Creates validation set
@@ -613,10 +613,10 @@ if __name__ == "__main__":
     #     args['use_unlabeled_samples'] = use_unlabeled_samples
     #     run(args)
     
-    print('\nOnly regression\n')
-    args['pred_type'] = 'reg'
-    run(args)
-    print('+' * 72)
+    # print('\nOnly regression\n')
+    # args['pred_type'] = 'reg'
+    # run(args)
+    # print('+' * 72)
     
     # args['pred_type'] = 'reg+class'
     # print('\nreg+class\n')
