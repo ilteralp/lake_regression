994d4066c04f85036b58b2713838f155990aaa9f
melike <milteralp@gmail.com>
2021-03-20 00:34:25 +0300

    Added fold setup dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,4 @@
 from .base_lake_dataset import BaseLakeDataset
 from .lake_1d_dataset import Lake1dDataset
 from .lake_2d_dataset import Lake2dDataset
+from .lake_2d_fold_dataset import Lake2dFoldDataset
