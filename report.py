f299e4c8e756c93ccf2defe16dc167b0c10bb7a6
melike <milteralp@gmail.com>
2021-03-31 15:55:40 +0300

    Added new field to report. Writes test dict to report with a loop. Fixed writing empty values and booleans.

--- report.py
+++ report.py
@@ -10,6 +10,7 @@ import os
 import os.path as osp
 from xlwt import Workbook
 import xlwt
+import math
 import constants as C
 
 class Report:
@@ -32,49 +33,69 @@ class Report:
         self.sheet.write(0, 3, 'PredType', self.header)
         self.sheet.write(0, 4, 'NumEpochs', self.header)
         self.sheet.write(0, 5, 'HasValSet', self.header)
-        self.sheet.write(0, 6, 'UseUnlabeled', self.header)
+        self.sheet.write(0, 6, 'UsedUnlabeled', self.header)
         self.sheet.write(0, 7, 'Seed', self.header)
         self.sheet.write(0, 8, 'PatchNorm', self.header)
         self.sheet.write(0, 9, 'RegNorm', self.header)
         self.sheet.write(0, 10, 'DateType', self.header)                         # Only meaningful in case of class and reg+class.
         self.sheet.write(0, 11, 'Model', self.header)
-        self.sheet.write(0, 12, 'ScoreName', self.header)
-        self.sheet.write(0, 13, 'LastEpochModelMean', self.header)              # For folded cases mean of score, o.w. score itself. 
-        self.sheet.write(0, 14, 'LastEpochModelStd', self.header)               # For folded cases std of score, o.w. 0.
-        self.sheet.write(0, 15, 'BestValLossModelMean', self.header)
-        self.sheet.write(0, 16, 'BestValLossModelStd', self.header)
-        self.sheet.write(0, 17, 'BestValScoreModelMean', self.header)
-        self.sheet.write(0, 18, 'BestValScoreModelStd', self.header)
-        self.sheet.write(0, 19, 'ScoreName', self.header)
-        self.sheet.write(0, 20, 'LastEpochModelMean', self.header)              # For folded cases mean of score, o.w. score itself. 
-        self.sheet.write(0, 21, 'LastEpochModelStd', self.header)               # For folded cases std of score, o.w. 0.
-        self.sheet.write(0, 22, 'BestValLossModelMean', self.header)
-        self.sheet.write(0, 23, 'BestValLossModelStd', self.header)
-        self.sheet.write(0, 24, 'BestValScoreModelMean', self.header)
-        self.sheet.write(0, 25, 'BestValScoreModelStd', self.header)
+        self.sheet.write(0, 12, 'ModelSplitLayer', self.header)
+        self.sheet.write(0, 13, 'ScoreName', self.header)
+        self.sheet.write(0, 14, 'LastEpochModelMean', self.header)              # For folded cases mean of score, o.w. score itself. 
+        self.sheet.write(0, 15, 'LastEpochModelStd', self.header)               # For folded cases std of score, o.w. 0.
+        self.sheet.write(0, 16, 'BestValLossModelMean', self.header)
+        self.sheet.write(0, 17, 'BestValLossModelStd', self.header)
+        self.sheet.write(0, 18, 'BestValScoreModelMean', self.header)
+        self.sheet.write(0, 19, 'BestValScoreModelStd', self.header)
+        self.sheet.write(0, 20, 'ScoreName', self.header)
+        self.sheet.write(0, 21, 'LastEpochModelMean', self.header)              # For folded cases mean of score, o.w. score itself. 
+        self.sheet.write(0, 22, 'LastEpochModelStd', self.header)               # For folded cases std of score, o.w. 0.
+        self.sheet.write(0, 23, 'BestValLossModelMean', self.header)
+        self.sheet.write(0, 24, 'BestValLossModelStd', self.header)
+        self.sheet.write(0, 25, 'BestValScoreModelMean', self.header)
+        self.sheet.write(0, 26, 'BestValScoreModelStd', self.header)
         # self.sheet.write(0, 25, 'Loss', self.header)                          # AWL or sum
 
+    """
+    """
+    def _test_result_to_sheet(self, test_result, rid, idx):
+        init_idx = idx
+        for score_name in test_result:
+            self.sheet.write(rid, idx, score_name)
+            idx = idx + 1
+            for model_name in test_result[score_name]:
+                for res in test_result[score_name][model_name]:
+                    val = test_result[score_name][model_name][res]
+                    if not math.isnan(val):                                     # Leave NaN values are blank in column
+                        self.sheet.write(rid, idx, '{:.4f}'.format(val))
+                    idx = idx + 1
+        if len(test_result) == 1:                                               # There are two score_names in the table, so don't return idx of next score_name. 
+            idx = idx + idx - init_idx
+        return idx
         
     """
     Adds given dataset, its score and model name to the report. 
     """
     def add(self, args, metrics):
         rid = len(self.sheet._Worksheet__rows)
-        test_result = metrics.get_mean_std_test_results()
         self.sheet.write(rid, 0, args['run_name'])
         self.sheet.write(rid, 1, args['fold_setup'])
         self.sheet.write(rid, 2, args['num_folds'])                             # Check for None. 
         self.sheet.write(rid, 3, args['pred_type'])
         self.sheet.write(rid, 4, args['max_epoch'])
-        self.sheet.write(rid, 5, args['create_val'])
-        self.sheet.write(rid, 6, args['use_unlabeled_samples'])
+        self.sheet.write(rid, 5, 'True' if args['create_val'] == 1 else 'False')
+        self.sheet.write(rid, 6, 'True' if args['use_unlabeled_samples'] == 1 else 'False')
         self.sheet.write(rid, 7, args['seed'])
-        self.sheet.write(rid, 8, args['patch_norm'])
-        self.sheet.write(rid, 9, args['reg_norm'])
-        self.sheet.write(rid, 10, args['date_type'])
+        self.sheet.write(rid, 8, 'True' if args['patch_norm'] == 1 else 'False')
+        self.sheet.write(rid, 9, 'True' if args['reg_norm'] == 1 else 'False')
+        self.sheet.write(rid, 10, '' if args['pred_type'] == 'reg' else args['date_type'])
         self.sheet.write(rid, 11, args['model'])
-        # self.sheet.write(rid, 12, score_name)
-
+        self.sheet.write(rid, 12, args['split_layer'] if args['model'] == 'eadan' else '')
+        test_result = metrics.get_mean_std_test_results()
+        print(test_result)
+        idx = self._test_result_to_sheet(test_result=test_result, rid=rid, idx=13)
+        print('Returned idx:', idx)
+        
     """
     Returns current report's id. 
     """
