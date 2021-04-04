51df7429ec4dafe89df8826793452b037de9b1b9
melike <milteralp@gmail.com>
2021-04-04 16:02:44 +0300

    Testing for temporal_year

--- train.py
+++ train.py
@@ -814,7 +814,8 @@ if __name__ == "__main__":
     report = Report()
     args['report_id'] = report.get_report_id()
     
-    for fold_setup in ['spatial', 'temporal_day', 'temporal_year', 'random']:
+    # for fold_setup in ['spatial', 'temporal_day', 'temporal_year', 'random']:
+    for fold_setup in ['temporal_year']:
         for pred_type in ['reg', 'reg+class']:
             for use_unlabeled_samples in [False, True]:
                 if pred_type == 'reg' and use_unlabeled_samples:
