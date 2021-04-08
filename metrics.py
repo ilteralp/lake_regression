7d7e66f6abfc2e57d8afd5fa6ef394650af5f590
melike <milteralp@gmail.com>
2021-04-08 21:25:38 +0300

    Implemented classification and regression metrics in Pytorch to calculate via GPU

--- metrics.py
+++ metrics.py
@@ -22,12 +22,13 @@ class Metrics:
         cross-validation or an int >0 for training with cross-validation. 
     """
     
-    def __init__(self, num_folds, device, pred_type):
+    def __init__(self, num_folds, device, pred_type, num_classes=None):
         self.num_folds = num_folds
         self.device = device
         self.pred_type = pred_type
         self.test_score_names = self._init_test_score_names()
         self.test_scores = self._init_test_scores()
+        self.num_classes = num_classes
         
     def _init_test_score_names(self):
         if self.pred_type == 'reg':
@@ -68,44 +69,171 @@ class Metrics:
                     result[score_name][model_name]['std'] = np.std(all_fold_scores)
         return result
                 
+    """
+    RMSE implementation with PyTorch. 
+    """
+    def __root_mean_squared_error(self, y_true, y_pred):
+        return torch.sqrt(torch.mean(torch.pow(y_true - y_pred, 2.0)))
+    
+    """
+    R2 score implementation with PyTorch.
+    From https://en.wikipedia.org/wiki/Coefficient_of_determination
+    """
+    def __r2_score(self, y_true, y_pred):
+        y_true_mean = torch.mean(y_true)
+        ss_tot = torch.sum((y_true - y_true_mean) ** 2)
+        ss_res = torch.sum((y_true - y_pred) ** 2)
+        r2 = 1 - ss_res / ss_tot
+        return r2
+    
+    """
+    MAE implementation with PyTorch. 
+    """
+    def __mean_absolute_error(self, y_true, y_pred):
+        mae = torch.nn.L1Loss()
+        return mae(y_pred, y_true)
+
+    """
+    Calculates confusion matrix. 
+    """
+    def __calc_conf_matrix(self, preds, labels):
+        conf_matrix = { i : {'TP' : 0, 'FP' : 0, 'FN' : 0, 'TN' : 0 } for i in range(self.num_classes)}
+        total_size = preds.nelement()
+        for i in range(self.num_classes):
+            TP = torch.sum((preds == i) * (labels == i)).item()
+            FP = torch.sum((preds == i)).item() - TP
+            FN = torch.sum((labels == i)).item() - TP
+            TN = total_size - TP - FP - FN
+            conf_matrix[i]['TP'] += TP
+            conf_matrix[i]['FP'] += FP
+            conf_matrix[i]['FN'] += FN
+            conf_matrix[i]['TN'] += TN
+        total_same = torch.sum(preds == labels).item()
+        return conf_matrix, total_same
+    
+    """
+    Calculates classification metrics with confusion matrix. 
+    """
+    def calc_class_metrics(self, preds, labels):
+        conf_matrix, total_same = self.__calc_conf_matrix(preds=preds, labels=labels)
+        kappa_sum, acc_sum, precision_sum, recall_sum, f1_sum = 0, 0, 0, 0, 0
+        scores_per_class = {}
+        for k, v in conf_matrix.items():
+            total = v['TP'] + v['FP'] + v['FN'] + v['TN']
+            assert total > 0, "Total of samples is 0 for class {}!".format(k)
             
-    # def _root_mean_squared_error(self, x, y):
-    #     return torch.sqrt(torch.mean(torch.pow(x-y, 2.0)))
+            """ ================ Accuracy ================ """
+            acc = (v['TP'] + v['TN']) / total
+            acc_sum += acc
+            
+            """ ================ Precision =============== """
+            precision = 0 if v['TP'] + v['FP'] == 0 else v['TP'] / (v['TP'] + v['FP'])
+            precision_sum += precision
+            
+            """ ================== Recall ================ """
+            recall = 0 if (v['TP'] + v['FN']) == 0 else v['TP'] / (v['TP'] + v['FN'])
+            recall_sum += recall
+            
+            """ ==================== F1 ================== """
+            f1 = 0 if (precision + recall) == 0 else 2 * (precision * recall) / (precision + recall)
+            f1_sum += f1
+            
+            """ ================== Kappa ================= """
+            p_obs = (v['TP'] + v['TN']) / total
+            p_yes = ((v['TP'] + v['FP']) / total) * ((v['TP'] + v['FN']) / total)
+            p_no = ((v['FN'] + v['TN']) / total) * ((v['FP'] + v['TN']) / total)
+            p_est = p_yes + p_no
+            kappa = -1.0 if p_est == 1.0 else (p_obs - p_est) / (1 - p_est)
+            kappa_sum += kappa
+            scores_per_class[k] = {'precision' : precision, 'recall' : recall, 'f1' : f1, 'kappa' : kappa}
+
+        scikit_acc = np.round(total_same / total, decimals=4)
+        scores = np.array([precision_sum, recall_sum, f1_sum, kappa_sum])
+        p_ov, r_ov, f_ov, k_ov = np.round(scores / self.num_classes, decimals=4)
+        scores_overall = {'accuracy' : scikit_acc, 'precision' : p_ov, 'recall' : r_ov, 'f1' : f_ov, 'kappa' : k_ov }
+        return scores_overall, scores_per_class
     
-    # def _r2_score(self):
-    #     pass
-    
-    # def _mean_absolute_error(self): 
-    #     pass
+    """
+    Calculates kappa via overall confusion matrix, not averaging class-wise. 
+    """
+    def calc_kappa_v2(self, conf_matrix):
+        TP, TN, FN, FP = 0, 0, 0, 0
+        for k, v in conf_matrix.items():
+            TN += v['TN']
+            TP += v['TP']
+            FN += v['FN']
+            FP += v['FP']
+            
+        total = TP + FP + FN + TN
+        obs_agr = TP + TN
+        exp_agr = (((TP + FP) * (TP + FN)) + ((FN + TN) * (FP + TN))) / total
+        if total == exp_agr:
+            raise Exception('Total of samples is equal to expected agreement!')
+        kappa = (obs_agr - exp_agr) / (total - exp_agr)
+        return np.round(kappa, decimals=4)
         
     """
     Calculates regression metrics for given batch.
     """
     def eval_reg_batch_metrics(self, preds, targets):
-        # rmse = self._root_mean_squared_error(preds, targets)
-        if self.device != 'cpu':
-            preds, targets = preds.cpu(), targets.cpu()
-        preds, targets = preds.detach().numpy().flatten(), targets.detach().numpy().flatten()
-        rmse = mean_squared_error(y_true=targets, y_pred=preds, squared=False)
-        r2 = r2_score(y_true=targets, y_pred=preds)
-        mae = mean_squared_error(y_true=targets, y_pred=preds)
+        rmse = self.__root_mean_squared_error(y_pred=preds, y_true=targets)
+        r2 = self.__r2_score(y_pred=preds, y_true=targets)
+        mae = self.__mean_absolute_error(y_pred=preds, y_true=targets)
+        # print('rmse: {:.4f}, r2: {:.4f}, mae: {:.4f}'.format(rmse, r2, mae))
+
+        """ Scikit metrics """
+        ###################################################################### 
+        # if self.device != 'cpu':
+        #     preds, targets = preds.cpu(), targets.cpu()
+        # preds, targets = preds.detach().numpy().flatten(), targets.detach().numpy().flatten()
+        # sk_rmse = mean_squared_error(y_true=targets, y_pred=preds, squared=False)
+        # sk_r2 = r2_score(y_true=targets, y_pred=preds)
+        # sk_mae = mean_absolute_error(y_true=targets, y_pred=preds)
+        # print('scikit rmse: {:.4f}, r2: {:.4f}, mae: {:.4f}'.format(sk_rmse, sk_r2, sk_mae))
+        ######################################################################
         return {'rmse' : rmse, 'r2' : r2, 'mae' : mae}
     
     """
     Calculates classification metrics for given batch.
     """
     def eval_class_batch_metrics(self, preds, targets):
-        preds = torch.argmax(preds, 1)
-        if self.device != 'cpu':
-            preds, targets = preds.cpu(), targets.cpu()
-        preds, targets = preds.detach().numpy().flatten(), targets.detach().numpy().flatten()
-        kappa = cohen_kappa_score(preds, targets)
-        f1 = f1_score(y_true=targets, y_pred=preds, average='macro')
-        acc = accuracy_score(y_true=targets, y_pred=preds)
+        preds = torch.argmax(preds, 1)                                          # No need to softmax before argmax, result is the same. 
+        scores_overall, scores_per_class = self.calc_class_metrics(preds=preds, labels=targets)
+        kappa, f1, acc = scores_overall['kappa'], scores_overall['f1'], scores_overall['accuracy']
+        # print('kappa: {:.4f}, f1: {:.4f}, acc: {:.4f}'.format(kappa, f1, acc))
+        
+        """ Scikit metrics """
+        ######################################################################
+        # if self.device != 'cpu':
+        #     preds, targets = preds.cpu(), targets.cpu()
+        # preds, targets = preds.detach().numpy().flatten(), targets.detach().numpy().flatten()
+        # sk_kappa = cohen_kappa_score(preds, targets)
+        # sk_f1 = f1_score(y_true=targets, y_pred=preds, average='macro')
+        # sk_acc = accuracy_score(y_true=targets, y_pred=preds)
+        # print('scikit kappa: {:.4f}, f1: {:.4f}, acc: {:.4f}'.format(sk_kappa, sk_f1, sk_acc))
+        ######################################################################
         return {'kappa' : kappa, 'f1' : f1, 'acc' : acc}
     
 """
 Ideas:
-1. Implement regression and classification scores in Pytorch. 
+X 1. Implement regression and classification scores in Pytorch. 
 """
+
+if __name__ == '__main__':
+    num_classes = 12
+    num_samples = 2
+    metrics = Metrics(num_folds=3, device='cpu', pred_type='reg+class', num_classes=num_classes)
     
+    """ Classification """
+    labels = torch.randint(0, num_classes, (num_samples,))
+    preds = torch.rand(num_samples, num_classes)
+    metrics.eval_class_batch_metrics(preds=preds, targets=labels)
+    
+    """ Regression """
+    # preds = torch.rand(num_samples)
+    # targets = torch.rand(num_samples)
+    # metrics.eval_reg_batch_metrics(preds=preds, targets=targets)
+    
+    
+
+
