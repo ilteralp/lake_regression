b38748905a403e3a36c9ce80f9419f167a6262a7
melike <milteralp@gmail.com>
2021-04-16 14:58:38 +0300

    Back to random debug

--- train.py
+++ train.py
@@ -980,7 +980,7 @@ if __name__ == "__main__":
     
     """ Create experiment params """
     loss_names = ['sum']
-    fold_setups = ['spatial']
+    fold_setups = ['random']
     pred_types = ['reg+class']
     using_unlabeled_samples = [True]
     date_types = ['month']
