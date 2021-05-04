b6b6b15bd86805b38740b3539e2e7ccf0837a8ae
melike <milteralp@gmail.com>
2021-05-04 22:52:44 +0300

    Fixed using same conv

--- models/ea_net.py
+++ models/ea_net.py
@@ -18,31 +18,44 @@ import constants as C
 from models import VerboseExecution
 
 class EANet(nn.Module):
-    def __init__(self, in_channels, num_classes=None):
+    def __init__(self, in_channels, num_classes=None, num_convs=4):
         super(EANet, self).__init__()
         self.in_channels = in_channels
         self.num_classes = num_classes
+        self.num_convs = num_convs - 1                                          # - 1 due to creating 1st conv layer separately.
         
-        self.pad = nn.ReflectionPad2d(padding=1)
-        self.act = nn.Tanh()
-        self.conv_first = nn.Conv2d(in_channels=self.in_channels, out_channels=64, kernel_size=3, stride=1)
-        self.conv = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
+        """ Convs """
+        self.conv1 = self.__make_layer(in_channels=self.in_channels, out_channels=64)
+        self.__make_layers()
         
-        self.fc1 = nn.Linear(in_features=64 * 3 * 3, out_features=128)
+        """ FCs """
+        self.fc1 = nn.Sequential(nn.Flatten(start_dim=1),
+                                 nn.Linear(in_features=64 * 3 * 3, out_features=128),
+                                 nn.Tanh())
         out_features = 1 if self.num_classes is None else self.num_classes
         self.fc2 = nn.Linear(in_features=128, out_features=out_features)
         
         """ Init weights """
         self._init_weights()
+
+    def __make_layers(self):
+        """ Using nn.Sequential """
+        self.convs = []
+        for i in range(self.num_convs):
+            conv = self.__make_layer(in_channels=64, out_channels=64)
+            self.convs.append(conv)
+            setattr(self, 'seq_conv{}'.format(i + 2), conv)                      # They start from 2, so add 2. 
+            
+    def __make_layer(self, in_channels, out_channels):
+        conv2d = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3)
+        layers = [nn.ReflectionPad2d(padding=1), conv2d, nn.Tanh()]
+        return nn.Sequential(*layers)
         
     def forward(self, x):
-        x = self.act(self.conv_first(self.pad(x)))                              # (bs, 12, 3, 3) -> (bs, 64, 3, 3)
-        x = self.act(self.conv(self.pad(x)))                                    # (bs, 64, 3, 3) -> (bs, 64, 3, 3)
-        x = self.act(self.conv(self.pad(x)))                                    # (bs, 64, 3, 3) -> (bs, 64, 3, 3)
-        x = self.act(self.conv(self.pad(x)))                                    # (bs, 64, 3, 3) -> (bs, 64, 3, 3)
-        
-        # x = self.act(self.fc1(x.view(x.shape[0], -1)))                          # (bs, 64 * 3 * 3) -> (bs, 128)
-        x = self.act(self.fc1(torch.flatten(x, start_dim=1)))
+        x = self.conv1(x)                                                       # (bs, 12, 3, 3) -> (bs, 64, 3, 3)
+        for conv in self.convs:                                                 # (bs, 64, 3, 3) -> (bs, 64, 3, 3)
+            x = conv(x)
+        x = self.fc1(x)                                                         # (bs, 64 * 3 * 3) -> (bs, 128)
         x = self.fc2(x)                                                         # (bs, 128) -> (bs, 1)
         return x
     
@@ -70,12 +83,12 @@ class EANet(nn.Module):
 #         layer.register_forward_hook(hook_fn)
     
 if __name__ == "__main__":
-    in_channels = 12
+    in_channels = 3
     net = EANet(in_channels=in_channels)
     # verbose_net = VerboseExecution(net)
     # # net.apply(weights_init)                                                     # Init weights
     
-    # inp = torch.randn(2, 12, 3, 3)
+    # inp = torch.randn(2, in_channels, 3, 3)
     # _ = verbose_net(inp)
     # # outp = net(inp)
     # # print(outp.shape)
