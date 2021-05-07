960547bb355808e5d814ae7caabde3aa1d3313e6
melike <milteralp@gmail.com>
2021-05-08 00:42:18 +0300

    Seeing effect of dropout (testing without dropout)

--- train.py
+++ train.py
@@ -987,8 +987,8 @@ if __name__ == "__main__":
     """ Create experiment params """
     loss_names = ['awl']
     fold_setups = ['random']
-    pred_types = ['reg', 'reg+class']
-    using_unlabeled_samples = [False, True]
+    pred_types = ['reg+class']
+    using_unlabeled_samples = [False]
     date_types = ['month']
     # split_layers = [*range(1, 6)]
     split_layers = [4]
@@ -1004,8 +1004,8 @@ if __name__ == "__main__":
         args['fold_setup'] = fold_setup
         args['pred_type'] = pred_type
         args['use_unlabeled_samples'] = unlabeled
-        args['num_folds'] = C.FOLD_SETUP_NUM_FOLDS[args['fold_setup']]
-        # args['num_folds'] = None
+        # args['num_folds'] = C.FOLD_SETUP_NUM_FOLDS[args['fold_setup']]
+        args['num_folds'] = None
         args['create_val'] = False if args['fold_setup'] == 'temporal_year' else True
         args['date_type'] = date_type
         args['split_layer'] = split_layer
