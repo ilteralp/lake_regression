807c3eea3e90e21b8806d513839851f27fe89047
melike <milteralp@gmail.com>
2021-05-23 18:37:36 +0300

    Testing with norm

--- train.py
+++ train.py
@@ -1048,8 +1048,8 @@ if __name__ == "__main__":
             'seed': seed,
             'test_per': 0.1,
             'lr': 0.0001,                                                       # From EA's model, default is 1e-2.
-            'patch_norm': False,                                                # Normalizes patches
-            'reg_norm': False,                                                  # Normalize regression values
+            'patch_norm': True,                                                # Normalizes patches
+            'reg_norm': True,                                                  # Normalize regression values
             'model': 'eaoriginal',                                                   # Model name, can be {dandadadan, eanet, eadan}.
             'use_test_as_val': True,                                            # Uses test set for validation. 
             'num_early_stop_epoch': 10,                                         # Number of consecutive epochs that model loss does not decrease. 
