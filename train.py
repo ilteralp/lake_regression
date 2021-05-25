98394fe826ca9a74c5fb443abf4a191e8e63ce68
melike <milteralp@gmail.com>
2021-05-26 01:05:00 +0300

    On test set

--- train.py
+++ train.py
@@ -1083,7 +1083,7 @@ if __name__ == "__main__":
             'patch_norm': True,                                                # Normalizes patches
             'reg_norm': True,                                                  # Normalize regression values
             'model': 'eadan',                                                   # Model name, can be {dandadadan, eanet, eadan}.
-            'use_test_as_val': False,                                            # Uses test set for validation. 
+            'use_test_as_val': True,                                            # Uses test set for validation. 
             'num_early_stop_epoch': 5,                                         # Number of consecutive epochs that model loss does not decrease. 
             
             'tr': {'batch_size': C.BATCH_SIZE, 'shuffle': True, 'num_workers': 4},
@@ -1117,8 +1117,8 @@ if __name__ == "__main__":
         args['use_unlabeled_samples'] = unlabeled
         # args['num_folds'] = C.FOLD_SETUP_NUM_FOLDS[args['fold_setup']]
         args['num_folds'] = None
-        args['create_val'] = False if args['fold_setup'] == 'temporal_year' else True
-        # args['create_val'] = False
+        # args['create_val'] = False if args['fold_setup'] == 'temporal_year' else True
+        args['create_val'] = False
         args['date_type'] = date_type
         args['split_layer'] = split_layer
         args['patch_size'] = patch_size
