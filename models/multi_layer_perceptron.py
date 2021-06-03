c156f31b1f9dc5a28e87204f54725f6b94744254
melike <milteralp@gmail.com>
2021-06-03 20:40:23 +0300

    Testing with new layers

--- models/multi_layer_perceptron.py
+++ models/multi_layer_perceptron.py
@@ -26,8 +26,8 @@ class MultiLayerPerceptron(nn.Module):
         self.out = nn.Linear(in_features=in_features, out_features=1)
 
     def __verify(self, cfg):
-        if cfg not in ['{}_hidden_layer'.format(i) for i in range(1, 7)]:
-            raise Exception('Configuration for multi layer perceptron can be one of {}'.format(['{}_hidden_layer'.format(i) for i in range(1, 5)]))
+        if cfg not in ['{}_hidden_layer'.format(i) for i in range(1, 9)]:
+            raise Exception('Configuration for multi layer perceptron can be one of {}'.format(['{}_hidden_layer'.format(i) for i in range(1, 9)]))
         
     def __make_fc(self, in_features, out_features, activation):
         fc = nn.Linear(in_features=in_features, out_features=out_features)
@@ -38,8 +38,8 @@ class MultiLayerPerceptron(nn.Module):
         self.fcs = []
         in_features = self.in_features
         for i, out_features in enumerate(self.cfg):
-            activation = 'tanh' if i == len(self.cfg) - 1 else 'relu'
-            # activation = 'tanh'
+            # activation = 'tanh' if i == len(self.cfg) - 1 else 'relu'
+            activation = 'tanh'
             fc = self.__make_fc(in_features=in_features, out_features=out_features, activation=activation)
             self.fcs.append(fc)
             setattr(self, 'fc{}'.format(i+1), fc)
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     in_features = in_channels * patch_size * patch_size
     inp = torch.randn(2, in_channels, patch_size, patch_size)
     # for i in range(1, 5):
-    i = 6
+    i = 8
     model = MultiLayerPerceptron(in_channels=in_channels, patch_size=patch_size, cfg='{}_hidden_layer'.format(i))
     # outp = model(inp)
     # print(model)
