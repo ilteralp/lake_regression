d869201c8bcc8d8427a68d1ad0cbf744ba272c65
melike <milteralp@gmail.com>
2021-06-19 02:03:01 +0300

    Testing with unlabeled samples

--- train.py
+++ train.py
@@ -1163,14 +1163,14 @@ if __name__ == "__main__":
     args['report_id'] = report.report_id
     
     """ Create experiment params """
-    loss_names = ['sum', 'awl']
+    loss_names = ['awl']
     fold_setups = ['random']
     pred_types = ['reg+class']
-    using_unlabeled_samples = [False]
+    using_unlabeled_samples = [True]
     date_types = ['month']
     # split_layers = [*range(1,3)]
     split_layers = [5]
-    patch_sizes = [5]
+    patch_sizes = [3]
     patch_norms = [False]
     
     # mlp_cfgs = ['{}_hidden_layer'.format(i) for i in range(7, 9)] if args['model'] == 'mlp' else None
