30bcf688f2ce5912cb97b01c73f96903f5a8a708
melike <milteralp@gmail.com>
2021-06-19 00:24:03 +0300

    Added EAOriginalDAN model

--- models/__init__.py
+++ models/__init__.py
@@ -7,3 +7,4 @@ from .ea_dan import EADAN
 from .ea_original import EAOriginal
 from .multi_layer_perceptron import MultiLayerPerceptron
 from .water_net import WaterNet
+from .ea_original_dan import EAOriginalDAN
