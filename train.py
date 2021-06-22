730062d70e11bd7869404c6c57c10da9ace0cb6d
melike <milteralp@gmail.com>
2021-06-22 20:38:30 +0300

    Using unlabeled samples

--- train.py
+++ train.py
@@ -1167,7 +1167,7 @@ if __name__ == "__main__":
     loss_names = ['sum']
     fold_setups = ['random']
     pred_types = ['reg+class']
-    using_unlabeled_samples = [False]
+    using_unlabeled_samples = [True]
     date_types = ['month']
     # split_layers = [*range(1,3)]
     split_layers = [3, 4, 5]
