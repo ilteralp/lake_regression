42a48d0f519055aa30c17ef9277fab7b5e3de291
melike <milteralp@gmail.com>
2021-06-27 23:45:46 +0300

    Single task baseline

--- train.py
+++ train.py
@@ -1166,8 +1166,8 @@ if __name__ == "__main__":
     """ Create experiment params """
     loss_names = ['sum']
     fold_setups = ['random']
-    pred_types = ['reg+class']
-    using_unlabeled_samples = [True]
+    pred_types = ['reg']
+    using_unlabeled_samples = [False]
     date_types = ['month']
     # split_layers = [*range(1,3)]
     split_layers = [4]
