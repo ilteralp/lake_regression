2af82f3266ca75ddda30670e38f8b66658463abc
melike <milteralp@gmail.com>
2021-06-30 15:25:30 +0300

    Testing batch size for unlabeled samples

--- constants.py
+++ constants.py
@@ -54,5 +54,5 @@ MLP_CFGS = {'1_hidden_layer' : [673],
 
 """ ===================== Model params ===================== """
 BATCH_SIZE = 64
-UNLABELED_BATCH_SIZE = 4568
+UNLABELED_BATCH_SIZE = 6852
 BASE_LR = 0.0001
