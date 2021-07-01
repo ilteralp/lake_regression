8df9aa57842d67223831f892d907f53997120225
melike <milteralp@gmail.com>
2021-07-01 12:25:46 +0300

    Increased epoch size for 2.2million unlabeled samples

--- constants.py
+++ constants.py
@@ -54,5 +54,5 @@ MLP_CFGS = {'1_hidden_layer' : [673],
 
 """ ===================== Model params ===================== """
 BATCH_SIZE = 64
-UNLABELED_BATCH_SIZE = 7994
+UNLABELED_BATCH_SIZE = 8192
 BASE_LR = 0.00005
