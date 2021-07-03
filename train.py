36b8bfee1aa86a42877c93c47a6424dafb9e13e3
melike <milteralp@gmail.com>
2021-07-03 17:31:54 +0300

    MTL on 503K unlabeled samples

--- train.py
+++ train.py
@@ -1184,10 +1184,10 @@ if __name__ == "__main__":
     args['report_id'] = report.report_id
     
     """ Create experiment params """
-    loss_names = ['sum']
+    loss_names = ['awl']
     fold_setups = ['random']
-    pred_types = ['reg']
-    using_unlabeled_samples = [False]
+    pred_types = ['reg+class']
+    using_unlabeled_samples = [True]
     date_types = ['month']
     # split_layers = [*range(1,3)]
     split_layers = [3]
