5c53999afd1879eb35cbb9bb7f7e4a0cc842d83c
melike <milteralp@gmail.com>
2021-07-03 18:36:46 +0300

    Added cases for unlabeled batch size due to confusion

--- constants.py
+++ constants.py
@@ -54,5 +54,8 @@ MLP_CFGS = {'1_hidden_layer' : [673],
 
 """ ===================== Model params ===================== """
 BATCH_SIZE = 64
-UNLABELED_BATCH_SIZE = 512
+if 'lake_mask_small.png' in MASK_PATH:
+    UNLABELED_BATCH_SIZE = 512
+elif 'lake_mask_small_45.png' in MASK_PATH:
+    UNLABELED_BATCH_SIZE = 4568
 BASE_LR = 0.00005
