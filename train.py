0cdb7b28e9352d4134be2e7e6af72cc2672bc113
melike <milteralp@gmail.com>
2021-07-03 17:51:17 +0300

    Baseline check

--- train.py
+++ train.py
@@ -1220,7 +1220,7 @@ if __name__ == "__main__":
         args['patch_norm'] = patch_norm
         args['reg_norm'] = reg_norm
         if args['pred_type'] == 'reg+class':
-            args['lr_reg'] = C.BASE_LR * 2
+            args['lr_reg'] = C.BASE_LR
             args['lr_class'] = C.BASE_LR
         # args['mlp_cfg'] = mlp_cfg
         print('loss_name: {}, {}, {}, use_unlabeled: {}, date_type: {}, split_layer: {}, patch_size: {}, patch_norm: {}, reg_norm: {}'.format(loss_name, fold_setup, pred_type, unlabeled, date_type, split_layer, patch_size, patch_norm, reg_norm))
