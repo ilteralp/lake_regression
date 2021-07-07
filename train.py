48664323c7b2ebf86c14d5b017b9579fe976f48c
melike <milteralp@gmail.com>
2021-07-07 23:03:32 +0300

    Best MTL with split_layer=4 and starting from fold=6 to continue experiment 2021_07_06__00_26_24

--- train.py
+++ train.py
@@ -1199,7 +1199,7 @@ if __name__ == "__main__":
     using_unlabeled_samples = [True]
     date_types = ['month']
     # split_layers = [*range(1,3)]
-    split_layers = [5]
+    split_layers = [4]
     patch_sizes = [3]
     patch_norms = [False]
     reg_norms = [True]
