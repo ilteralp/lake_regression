65a36cdd59b8a6aecdb091c4b29dcff78f3a5b3c
melike <milteralp@gmail.com>
2021-07-12 20:35:54 +0300

    MTL with random setup on 2.3M samples with 256/32/32 split

--- train.py
+++ train.py
@@ -1188,7 +1188,7 @@ if __name__ == "__main__":
             # 'patch_norm': True,                                                # Normalizes patches
             # 'reg_norm': True,                                                  # Normalize regression values
             'model': 'eaoriginaldan',                                                   # Model name, can be {dandadadan, eanet, eadan}.
-            'use_test_as_val': True,                                            # Uses test set for validation. 
+            'use_test_as_val': False,                                            # Uses test set for validation. 
             'num_early_stop_epoch': 3,                                         # Number of consecutive epochs that model loss does not decrease. 
             'sample_ids_from_run': SAMPLE_IDS_FROM_RUN_NAME,
             'reshape_to_mosaic': False,
@@ -1205,7 +1205,7 @@ if __name__ == "__main__":
     
     """ Create experiment params """
     loss_names = ['awl']
-    fold_setups = ['temporal_day']
+    fold_setups = ['random']
     pred_types = ['reg+class']
     using_unlabeled_samples = [True]
     date_types = ['month']
@@ -1231,10 +1231,10 @@ if __name__ == "__main__":
         args['fold_setup'] = fold_setup
         args['pred_type'] = pred_type
         args['use_unlabeled_samples'] = unlabeled
-        # args['num_folds'] = C.FOLD_SETUP_NUM_FOLDS[args['fold_setup']]
-        args['num_folds'] = None
-        # args['create_val'] = False if args['fold_setup'] == 'temporal_year' else True
-        args['create_val'] = False
+        args['num_folds'] = C.FOLD_SETUP_NUM_FOLDS[args['fold_setup']]
+        # args['num_folds'] = None
+        args['create_val'] = False if args['fold_setup'] == 'temporal_year' else True
+        # args['create_val'] = False
         args['date_type'] = date_type
         args['split_layer'] = split_layer
         args['patch_size'] = patch_size
