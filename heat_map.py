e50958f492e61b504e6f698909d687d13936f138
melike <milteralp@gmail.com>
2021-07-19 22:59:16 +0300

    Plots all heatmaps in a figure with one cbar

--- heat_map.py
+++ heat_map.py
@@ -18,15 +18,21 @@ from torch.utils.data import DataLoader, Subset
 from datasets import Lake2dDataset
 from train import create_model, get_reg_min_max
 from baseline import load_fold_sample_ids_args
+import constants as C
 
 HEAT_MAP_PARAMS = {'linewidth': 0,
                    'cmap': 'Spectral_r',
                    'xticklabels': False,
-                   'yticklabels': False}
+                   'yticklabels': False,
+                   'cbar': False,
+                   'square': True}
 
 def generate_rand_heatmap():
-    uniform_data = np.random.rand(600, 600)
+    uniform_data = np.random.rand(300, 300)
     ax = sns.heatmap(uniform_data, **HEAT_MAP_PARAMS)
+    PATH = '/home/melike/rs/balik_golu/heatmaps/random_heatmap.png'
+    # plt.savefig(PATH, format='pdf', dpi=1200)
+    plt.savefig(PATH)
     plt.show()
     
 """
@@ -43,13 +49,17 @@ def calc_image_sample_ids(img_id, len_labeled_dataset, len_unlabeled_dataset):
 Takes a model and subsets created from given image id, predicts each pixel's 
 regression value. 
 """
-def predict(model, loader, heatmap, reg_min, reg_max):
+def predict(model, args, loader, heatmap, reg_min, reg_max):
     model.eval()
     with torch.no_grad():
+        print('len loader:', len(loader))
         for batch_id, data in enumerate(loader):
             patches, _, _, (img_ids, pxs, pys) = data
             patches = patches.to(args['device'])
             reg_preds, _ = model(patches)
+            unnorm_regs = [unnorm_reg_val(r=r, reg_min=reg_min, reg_max=reg_max) for r in reg_preds]
+            # heatmap[pxs, pys] = unnorm_regs
+            # print('pxs: {}, pys: {}, regs: {}'.format(pxs, pys, unnorm_regs))
             for j in range(len(reg_preds)):
                 unnorm_reg = unnorm_reg_val(r=reg_preds[j], reg_min=reg_min, reg_max=reg_max)
                 heatmap[pxs[j]][pys[j]] = unnorm_reg
@@ -61,7 +71,7 @@ def predict(model, loader, heatmap, reg_min, reg_max):
 Takes an image, labeled and unlabeled datasets and model. Predicts image's 
 regression values with the model and creates its heat map. 
 """
-def generate_heatmap(img_id, labeled_dataset, unlabeled_dataset, model, reg_min, reg_max):
+def generate_heatmap(img_id, labeled_dataset, unlabeled_dataset, model, reg_min, reg_max, args):
     labeled_ids, unlabeled_ids = calc_image_sample_ids(img_id=img_id,                                   # Retrieve image's sample ids
                                                        len_labeled_dataset=len(labeled_dataset),
                                                        len_unlabeled_dataset=len(unlabeled_dataset))
@@ -74,26 +84,30 @@ def generate_heatmap(img_id, labeled_dataset, unlabeled_dataset, model, reg_min,
     heatmap = np.zeros([650, 650])
     for loader in [labeled_loader, unlabeled_loader]:
         heatmap = predict(model=model, loader=loader, heatmap=heatmap, 
-                          reg_min=reg_min, reg_max=reg_max)
+                          reg_min=reg_min, reg_max=reg_max, args=args)
+    return heatmap
         
-    ax = sns.heatmap(heatmap, **HEAT_MAP_PARAMS)
-    plt.show()
+    # ax = sns.heatmap(heatmap, **HEAT_MAP_PARAMS, vmin=11.04, vmax=108.35)
+    # plt.show()
     # train'den normalizasyon degerlerini al. 
     
-
-# """
-# Takes trained model's name and loads its args in order to creatie 
-# datasets for prediction. 
-# """
-# def load_args(run_name):
-#     run_path = osp.join(os.getcwd(), 'runs', run_name)
-#     args_path = osp.join(run_path, 'args.txt')
-#     if not osp.isfile(args_path):
-#         raise Exception('Given args file could not be found on {}.'.format(args_path))
-#     with open(args_path, 'rb') as f:                                            # Load args        
-#         args = eval(f.read())
-#     verify_args(args=args)
-#     return args
+"""
+Plots given heatmaps
+"""
+def plot(heatmaps, args, img_ids):
+    if len(heatmaps) not in [4, 12]:
+        raise Exception('Number of heatmaps can be 4 or 12. Given: {}'.format(len(heatmaps)))
+    fig, axn = plt.subplots(len(heatmaps) // 4, 4, sharey=True)
+    cbar_ax = fig.add_axes([.9, .3, .02, .4])
+    cbar_ax.tick_params(size=0)
+    
+    for i, ax in enumerate(axn.flat):
+        sns.heatmap(heatmaps[i], ax=ax, cbar=i == 0, vmin=11.04, vmax=108.35,
+                    square=True, xticklabels=False, yticklabels=False,
+                    cbar_ax=None if i else cbar_ax)
+    fig.tight_layout(rect=[0, 0, .9, 1])
+    heatmap_path = osp.join(C.ROOT_DIR, 'heatmaps', args['run_name'] + ''.join(['_{}'.format(str(i)) for i in img_ids]) + '.pdf')
+    plt.savefig(heatmap_path, format='pdf', dpi=600)
 
 """
 Verify args.
@@ -138,10 +152,10 @@ def unnorm_reg_val(r, reg_min, reg_max):
     return (reg_max - reg_min) * (r + 1) / 2 + reg_min
     
 if __name__ == "__main__":
-    # generate_rand_heatmap()
-    # RUN_NAME = '2021_07_18__13_56_46'
-    RUN_NAME = '2021_07_09__14_10_23'
+    RUN_NAME = '2021_07_18__13_56_46'
+    # RUN_NAME = '2021_07_09__14_10_23'
     fold_sample_ids, args = load_fold_sample_ids_args(run_name=RUN_NAME)
+    args['test']['batch_size'] = 32
     fold = 0
     
     """ Load model and datasets """
@@ -153,18 +167,25 @@ if __name__ == "__main__":
     unlabeled_dataset = Lake2dDataset(learning='unlabeled', **dataset_params)
     
     reg_min, reg_max = get_fold_reg_minmax(fold=fold,
-                                       fold_sample_ids=fold_sample_ids, 
-                                       labeled_dataset=labeled_dataset)
+                                           fold_sample_ids=fold_sample_ids, 
+                                           labeled_dataset=labeled_dataset)
     print('reg values, min: {}, max: {}'.format(reg_min, reg_max))
     # for r in [-1.0, 0, 1.0]:
     #     unnorm_r = unnorm_reg_val(r=r, reg_min=reg_min, reg_max=reg_max)
     #     print('{} became {}'.format(r, unnorm_r))
     
     """ Generate heatmap for given image """
-    img_id = 3
-    verify_image_id(img_id=img_id)
-    generate_heatmap(img_id=img_id, model=model,
-                      labeled_dataset=labeled_dataset, 
-                      unlabeled_dataset=unlabeled_dataset, 
-                      reg_min=reg_min, reg_max=reg_max)
+    heatmaps = []
+    img_ids = [3, 8, 12, 16]
+    for img_id in img_ids:
+        verify_image_id(img_id=img_id)
+        heatmap = generate_heatmap(img_id=img_id, model=model,
+                                   labeled_dataset=labeled_dataset, 
+                                   unlabeled_dataset=unlabeled_dataset, 
+                                   reg_min=reg_min, reg_max=reg_max, args=args)
+        heatmaps.append(heatmap)
+    
+    """ Plot heatmaps """
+    plot(heatmaps=heatmaps, args=args, img_ids=img_ids)
+    # generate_rand_heatmap()
     
