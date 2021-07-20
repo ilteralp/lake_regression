818d4c99482133625d3a0a35857e4d25b77c1ec4
melike <milteralp@gmail.com>
2021-07-20 13:18:55 +0300

    Removed tightening

--- heat_map.py
+++ heat_map.py
@@ -108,8 +108,8 @@ def plot(heatmaps, args, img_ids):
                     square=True, xticklabels=False, yticklabels=False,
                     cmap='Spectral_r', vmin=11.04, vmax=108.35,
                     cbar_ax=None if i else cbar_ax)
-    fig.tight_layout(rect=[0, 0, .9, 1])
-    heatmap_path = osp.join(C.ROOT_DIR, 'heatmaps', args['run_name'] + '_vmin_img_ids_' + ''.join(['_{}'.format(str(i)) for i in img_ids]) + '.pdf')
+    # fig.tight_layout(rect=[0, 0, .9, 1])
+    heatmap_path = osp.join(C.ROOT_DIR, 'heatmaps', args['run_name'] + '_untight_vmin_img_ids_' + ''.join(['_{}'.format(str(i)) for i in img_ids]) + '.pdf')
     # plt.savefig(heatmap_path, format='pdf', dpi=300)
     plt.savefig(heatmap_path, format='pdf')
 
