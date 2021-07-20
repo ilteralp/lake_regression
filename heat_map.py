cf1ee4a3484b094b398184e953022d5abf74030b
melike <milteralp@gmail.com>
2021-07-20 13:54:00 +0300

    Checking another batch of images...

--- heat_map.py
+++ heat_map.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     #     print('{} became {}'.format(r, unnorm_r))
     
     """ Generate heatmap for given image """
-    ids = [[6, 7], [8, 9], [10, 11], [12, 13], [14, 15]]
+    ids = [[16, 17], [18, 19], [20, 21], [22, 23], [24, 25]]
     for img_ids in ids:
         heatmaps = []
         # img_ids = [9, 13, 17, 23]
