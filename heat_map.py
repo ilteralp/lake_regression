568261d5a210a1fc4d60a81665d194132b0f3cef
melike <milteralp@gmail.com>
2021-07-21 14:25:10 +0300

    All in one file

--- heat_map.py
+++ heat_map.py
@@ -98,7 +98,7 @@ def plot(heatmaps, args, img_ids):
     # if len(heatmaps) not in [4, 12]:
     #     raise Exception('Number of heatmaps can be 4 or 12. Given: {}'.format(len(heatmaps)))
     # fig, axn = plt.subplots(len(heatmaps) // 4, 4, sharey=True)
-    fig, axn = plt.subplots(1, 2, sharey=True)
+    fig, axn = plt.subplots(8, 4, sharey=True)
     # cbar_ax = fig.add_axes([.9, .3, .02, .4])
     cbar_ax = fig.add_axes([.9, .355, .02, .29])
     cbar_ax.tick_params(size=0)
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     #     print('{} became {}'.format(r, unnorm_r))
     
     """ Generate heatmap for given image """
-    ids = [[0, 1], [2, 1], [4, 1], [6, 1], [8, 1], [10, 1], [12, 1], [14, 1], [16, 1], [18, 1], [20, 1], [22, 1], [24, 1], [26, 1], [28, 1]]
+    ids = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]]
     for img_ids in ids:
         heatmaps = []
         # img_ids = [9, 13, 17, 23]
