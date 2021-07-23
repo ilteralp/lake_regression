4c2f2e7c1f7a986341b9599ca94e76c4fca6ff57
melike <milteralp@gmail.com>
2021-07-23 18:24:48 +0300

    AWL vs sum

--- train.py
+++ train.py
@@ -1204,13 +1204,13 @@ if __name__ == "__main__":
     args['report_id'] = report.report_id
     
     """ Create experiment params """
-    loss_names = ['awl']
+    loss_names = ['sum']
     fold_setups = ['random']
     pred_types = ['reg+class']
     using_unlabeled_samples = [True]
     date_types = ['month']
     # split_layers = [*range(1,3)]
-    split_layers = [2]
+    split_layers = [4]
     patch_sizes = [3]
     patch_norms = [False]
     reg_norms = [True]
