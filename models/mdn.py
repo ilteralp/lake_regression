9c50a7cbdd4e3328ccf7b2321ac7693aa71f0cdb
melike <milteralp@gmail.com>
2021-10-29 11:54:36 +0300

    Adapted model

--- models/mdn.py
+++ models/mdn.py
@@ -34,17 +34,18 @@ class MDN(nn.Module):
             Gaussian.
     """
 
-    def __init__(self, in_features, out_features, num_gaussians):
+    def __init__(self, in_channels, patch_size, out_features, num_gaussians):
         super(MDN, self).__init__()
-        self.in_features = in_features
+        # self.in_features = in_features
+        self.in_features = in_channels * patch_size * patch_size
         self.out_features = out_features
         self.num_gaussians = num_gaussians
         self.pi = nn.Sequential(
-            nn.Linear(in_features, num_gaussians),
+            nn.Linear(self.in_features, num_gaussians),
             nn.Softmax(dim=1)
         )
-        self.sigma = nn.Linear(in_features, out_features * num_gaussians)
-        self.mu = nn.Linear(in_features, out_features * num_gaussians)
+        self.sigma = nn.Linear(self.in_features, out_features * num_gaussians)
+        self.mu = nn.Linear(self.in_features, out_features * num_gaussians)
 
     def forward(self, minibatch):
         pi = self.pi(minibatch)
@@ -54,46 +55,46 @@ class MDN(nn.Module):
         mu = mu.view(-1, self.num_gaussians, self.out_features)
         return pi, sigma, mu
 
+    @staticmethod
+    def gaussian_probability(sigma, mu, target):
+        """Returns the probability of `target` given MoG parameters `sigma` and `mu`.
+        Arguments:
+            sigma (BxGxO): The standard deviation of the Gaussians. B is the batch
+                size, G is the number of Gaussians, and O is the number of
+                dimensions per Gaussian.
+            mu (BxGxO): The means of the Gaussians. B is the batch size, G is the
+                number of Gaussians, and O is the number of dimensions per Gaussian.
+            target (BxI): A batch of target. B is the batch size and I is the number of
+                input dimensions.
+        Returns:
+            probabilities (BxG): The probability of each point in the probability
+                of the distribution in the corresponding sigma/mu index.
+        """
+        target = target.unsqueeze(1).expand_as(sigma)
+        ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((target - mu) / sigma)**2) / sigma
+        return torch.prod(ret, 2)
+    
+    @staticmethod
+    def mdn_loss(pi, sigma, mu, target):
+        """Calculates the error, given the MoG parameters and the target
+        The loss is the negative log likelihood of the data given the MoG
+        parameters.
+        """
+        prob = pi * MDN.gaussian_probability(sigma, mu, target)
+        nll = -torch.log(torch.sum(prob, dim=1))
+        return torch.mean(nll)
 
-def gaussian_probability(sigma, mu, target):
-    """Returns the probability of `target` given MoG parameters `sigma` and `mu`.
-    Arguments:
-        sigma (BxGxO): The standard deviation of the Gaussians. B is the batch
-            size, G is the number of Gaussians, and O is the number of
-            dimensions per Gaussian.
-        mu (BxGxO): The means of the Gaussians. B is the batch size, G is the
-            number of Gaussians, and O is the number of dimensions per Gaussian.
-        target (BxI): A batch of target. B is the batch size and I is the number of
-            input dimensions.
-    Returns:
-        probabilities (BxG): The probability of each point in the probability
-            of the distribution in the corresponding sigma/mu index.
-    """
-    target = target.unsqueeze(1).expand_as(sigma)
-    ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((target - mu) / sigma)**2) / sigma
-    return torch.prod(ret, 2)
-
-
-def mdn_loss(pi, sigma, mu, target):
-    """Calculates the error, given the MoG parameters and the target
-    The loss is the negative log likelihood of the data given the MoG
-    parameters.
-    """
-    prob = pi * gaussian_probability(sigma, mu, target)
-    nll = -torch.log(torch.sum(prob, dim=1))
-    return torch.mean(nll)
-
-
-def sample(pi, sigma, mu):
-    """Draw samples from a MoG.
-    """
-    # Choose which gaussian we'll sample from
-    pis = Categorical(pi).sample().view(pi.size(0), 1, 1)
-    # Choose a random sample, one randn for batch X output dims
-    # Do a (output dims)X(batch size) tensor here, so the broadcast works in
-    # the next step, but we have to transpose back.
-    gaussian_noise = torch.randn(
-        (sigma.size(2), sigma.size(0)), requires_grad=False)
-    variance_samples = sigma.gather(1, pis).detach().squeeze()
-    mean_samples = mu.detach().gather(1, pis).squeeze()
-    return (gaussian_noise * variance_samples + mean_samples).transpose(0, 1)
+    @staticmethod 
+    def sample(pi, sigma, mu):
+        """Draw samples from a MoG.
+        """
+        # Choose which gaussian we'll sample from
+        pis = Categorical(pi).sample().view(pi.size(0), 1, 1)
+        # Choose a random sample, one randn for batch X output dims
+        # Do a (output dims)X(batch size) tensor here, so the broadcast works in
+        # the next step, but we have to transpose back.
+        gaussian_noise = torch.randn(
+            (sigma.size(2), sigma.size(0)), requires_grad=False)
+        variance_samples = sigma.gather(1, pis).detach().squeeze()
+        mean_samples = mu.detach().gather(1, pis).squeeze()
+        return (gaussian_noise * variance_samples + mean_samples).transpose(0, 1)
