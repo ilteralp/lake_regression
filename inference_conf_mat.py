9f183760a65e95b9350d54211c3327c814e43f02
melike <milteralp@gmail.com>
2021-12-13 19:15:32 +0300

    Returns unlabeled dataset as expected

--- inference_conf_mat.py
+++ inference_conf_mat.py
@@ -11,19 +11,32 @@ import torch
 from torch.utils.data import Dataset, random_split, Subset, DataLoader
 from os import path as osp
 import constants as C
-from datasets import Lake2dFoldDataset
+from datasets import Lake2dFoldDataset, Lake2dDataset
 from metrics import Metrics
 from models import DandadaDAN, EANet, EADAN, EAOriginal, MultiLayerPerceptron, WaterNet, EAOriginalDAN, MDN, MaruMDN
 from losses import AutomaticWeightedLoss
 from train import _test, create_model
 from inference import load_model, get_test_loader_args
+from baseline import load_data, load_args
 
+
+"""
+Returns unlabeled datasets. 
+"""
+def get_unlabeled_dataset_loader(args):
+    unlabeled_set = Lake2dDataset(learning='unlabeled', date_type=args['date_type'], 
+                              patch_size=args['patch_size'], 
+                              reshape_to_mosaic=args['reshape_to_mosaic'])
+    unlabeled_loader = DataLoader(unlabeled_set, **args['unlabeled'])
+    return unlabeled_loader
 
 """
 Generates confusion matrix for given fold. 
 """
 def generate_conf_mat(run_name, best_run_name, model_name, best_fold):
-    test_loader, args = get_test_loader_args(run_name, best_run_name, best_fold, unlabeled=True) # Load unlabeled samples, because conf.mat is filled with 1's with labeled one. 
+    # test_loader, args = get_test_loader_args(run_name, best_run_name, best_fold, unlabeled=True) # Load unlabeled samples, because conf.mat is filled with 1's with labeled one. 
+    args = load_args(best_run_name)
+    test_loader = get_unlabeled_dataset_loader(args)
     model = load_model(model_name, best_fold, args)
     # model = create_model(args)                                                  # Testing with untrained model. 
     metrics = Metrics(num_folds=args['num_folds'], device=args['device'].type, 
